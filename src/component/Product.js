ef299527b5451d28df5af53f487aa8bf6e3b977b
vishal-blok <vishal@blokument.com>
2024-01-17 21:56:29 +0530

    fix api

--- src/component/Product.js
+++ src/component/Product.js
@@ -10,7 +10,7 @@ const Product = () => {
 
 
   const getData = () => {
-    axios.get("http://fakestoreapi.com/products")
+    axios.get("http://fakestoreapi.com/products/")
       .then((res) => {
         if (res.status === 200) {
           console.log("api avaiable", res.data);
